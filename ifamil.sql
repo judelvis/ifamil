d53fd9edaf2e0e0df2ce3153afe4f774d4340074
judelvis <jud.prog@gmail.com>
2015-09-05 14:30:26 -0430

    config

--- ifamil.sql
+++ ifamil.sql
@@ -3,7 +3,7 @@
 -- Server version:               5.5.37-0ubuntu0.13.10.1 - (Ubuntu)
 -- Server OS:                    debian-linux-gnu
 -- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2015-09-03 19:00:25
+-- Date/time:                    2015-09-05 14:30:12
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -33,10 +33,13 @@ CREATE TABLE IF NOT EXISTS `t_categoria` (
   `categoria` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `descrip` text COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`oid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
--- Dumping data for table ifamil.t_categoria: ~0 rows (approximately)
+-- Dumping data for table ifamil.t_categoria: ~2 rows (approximately)
 /*!40000 ALTER TABLE `t_categoria` DISABLE KEYS */;
+INSERT INTO `t_categoria` (`oid`, `categoria`, `descrip`) VALUES
+	(1, 'Viaje con nosotros', '*Venta de Boletos Aéreos Nacionales e Internacionales<br>'),
+	(2, 'Paquetes', 'algo2');
 /*!40000 ALTER TABLE `t_categoria` ENABLE KEYS */;
 
 
@@ -55,6 +58,40 @@ CREATE TABLE IF NOT EXISTS `t_datos` (
 /*!40000 ALTER TABLE `t_datos` ENABLE KEYS */;
 
 
+-- Dumping structure for table ifamil.t_empresa
+DROP TABLE IF EXISTS `t_empresa`;
+CREATE TABLE IF NOT EXISTS `t_empresa` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nombre` text COLLATE latin1_spanish_ci NOT NULL,
+  `imagen` text COLLATE latin1_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- Dumping data for table ifamil.t_empresa: ~0 rows (approximately)
+/*!40000 ALTER TABLE `t_empresa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_empresa` ENABLE KEYS */;
+
+
+-- Dumping structure for table ifamil.t_galeria
+DROP TABLE IF EXISTS `t_galeria`;
+CREATE TABLE IF NOT EXISTS `t_galeria` (
+  `oid` int(11) NOT NULL AUTO_INCREMENT,
+  `oidpor` int(11) NOT NULL,
+  `imagen` text COLLATE utf8_spanish_ci NOT NULL,
+  `titulo` text COLLATE utf8_spanish_ci NOT NULL,
+  `detalle` text COLLATE utf8_spanish_ci NOT NULL,
+  `fecha` date NOT NULL,
+  `modificado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `enlace` text COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`oid`),
+  KEY `oidpor` (`oidpor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- Dumping data for table ifamil.t_galeria: ~0 rows (approximately)
+/*!40000 ALTER TABLE `t_galeria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_galeria` ENABLE KEYS */;
+
+
 -- Dumping structure for table ifamil.t_noticias
 DROP TABLE IF EXISTS `t_noticias`;
 CREATE TABLE IF NOT EXISTS `t_noticias` (
@@ -67,10 +104,12 @@ CREATE TABLE IF NOT EXISTS `t_noticias` (
   `resumen` text NOT NULL,
   `modificado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`oid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Dumping data for table ifamil.t_noticias: ~0 rows (approximately)
+-- Dumping data for table ifamil.t_noticias: ~1 rows (approximately)
 /*!40000 ALTER TABLE `t_noticias` DISABLE KEYS */;
+INSERT INTO `t_noticias` (`oid`, `imagen`, `titulo`, `descrip`, `enlace`, `fecha`, `resumen`, `modificado`) VALUES
+	(1, '0 214_1.jpg', 'Noticia 1', 'Detalle Noticia 1', '', '2015-09-04', 'Resumen noticia 1', '2015-09-04 18:18:39');
 /*!40000 ALTER TABLE `t_noticias` ENABLE KEYS */;
 
 
@@ -87,10 +126,12 @@ CREATE TABLE IF NOT EXISTS `t_portafolio` (
   `estatus` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `oidcat` (`oidcat`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
--- Dumping data for table ifamil.t_portafolio: ~0 rows (approximately)
+-- Dumping data for table ifamil.t_portafolio: ~1 rows (approximately)
 /*!40000 ALTER TABLE `t_portafolio` DISABLE KEYS */;
+INSERT INTO `t_portafolio` (`id`, `oidcat`, `titulo`, `descrip`, `resumen`, `fecha`, `modificado`, `estatus`) VALUES
+	(1, 1, 'Viaje con nosotros', '*Venta de Boletos Aéreos Nacionales e Internacionales<br>', '*Venta de Boletos Aéreos Nacionales e Internacionales<br>\r\nY algo mas\r\n', '2015-09-04', '2015-09-04 16:35:37', 1);
 /*!40000 ALTER TABLE `t_portafolio` ENABLE KEYS */;
 
 
@@ -107,7 +148,7 @@ CREATE TABLE IF NOT EXISTS `t_usuarios` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table ifamil.t_usuarios: ~0 rows (approximately)
+-- Dumping data for table ifamil.t_usuarios: ~1 rows (approximately)
 /*!40000 ALTER TABLE `t_usuarios` DISABLE KEYS */;
 INSERT INTO `t_usuarios` (`id`, `documento`, `nombre`, `apellido`, `login`, `clave`, `nivel`) VALUES
 	(1, '1', 'Administrador', 'Administrador', 'admin', '202cb962ac59075b964b07152d234b70', 0);
