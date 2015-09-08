34586bc89ea51cbf479f5484eec6179279f92f1e
judelvis <jud.prog@gmail.com>
2015-09-08 14:15:18 -0430

    config

---
+++ application/views/principal/paralax.php
@@ -0,0 +1,138 @@
+    <link rel='stylesheet' type="text/css" href="<?php echo __PARALAX__?>style/style.css"/>
+    <link rel='stylesheet' type="text/css" href="<?php echo __PARALAX__?>css/scroller.css"/>
+    <link rel='stylesheet' type="text/css" href="<?php echo __PARALAX__?>css/dzsparallaxer.css"/>
+<body>
+<section class="mcon-otherdemos " style="padding-top: 160px;">
+    <div class="container">
+        <div class="row">
+            <!--
+            <div class="col-md-3 border-box" style="padding:15px 30px; text-align: center;">
+                <a href="index.html" title="waveform" class="active button-switcher" style="display: inline-block"><img src="img/switcher1.png"/></a><a class="button-switcher" title="with dynamic spectrum" href="index.html?spectrum=on" style="display: inline-block"><img src="img/switcher2.png"/></a>
+            </div>
+            -->
+            <div class="col-md-3"> </div>
+            <div class="col-md-9 border-box" style="padding:0px;">
+                <h1 class="hero-heading">Bonus Separators.</h1>
+            </div>
+        </div>
+    </div>
+</section>
+<style>
+    #prx1 .color-bg, #as0 .color-bg, .separator-gray .color-bg{ fill: #fafafa;
+    }
+</style>
+<div id="prx1" class="dzsparallaxer auto-init" data-options='{  mode_scroll: "fromtop", animation_duration: "50", direction: "reverse",js_breakout: "off", settings_movexaftermouse: "on"}' style="">
+
+    <div class="divimage dzsparallaxer--target " style="width: 130%; height: 530px; background-image: url(<?php echo __PARALAX__?>img/img2.jpg)">
+    </div>
+
+    <div class=" center-it" style="font-style:normal;">
+        <center><br>
+            <a class="twitter-timeline" href="https://twitter.com/ifamil_ca"
+               data-widget-id="640861362185502720" data-tweet-limit="1" data-chrome="noheader noborder transparent">Tweets
+                por el @ifamil_ca.</a>
+            <script>!function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                    if (!d.getElementById(id)) {
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = p + "://platform.twitter.com/widgets.js";
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }
+                }(document, "script", "twitter-wjs");</script>
+        </center>
+    </div>
+
+
+</div>
+<div class="main_bg"><!-- start main -->
+    <div class="container">
+        <div class="main_grid">
+            <div class="title">
+                <hr class="team_hr team_hr_left hr_gray"/>
+                <center><span class="line"></span>
+
+                    <h1>Servicios</h1></center>
+                <hr class="team_hr team_hr_left hr_gray"/>
+            </div>
+            <div class="span_of_4"><!-- start span_of_4 -->
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span class="active"><i class="fa fa-suitcase "></i></span>
+
+                        <h3>Paquetes</h3>
+
+                        <p>Realiza tu solicitud de paquetes ahora.</p>
+
+                        <div class="read_more">
+                            <a class="btn btn-2 active" href="single-page.html">Ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span class="active"><i class="fa fa-plane"></i></span>
+
+                        <h3>Reservación</h3>
+
+                        <p>Reserva tu vuelo ya con nosotros.</p>
+
+                        <div class="read_more">
+                            <a class="btn btn-2b" href="single-page.html">Ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span class="active"><i class="fa fa-group"></i></span>
+
+                        <h3>Dejanos tu opinion</h3>
+
+                        <p>Dejanos tu opinion.</p>
+
+                        <div class="read_more">
+                            <a class="btn  btn-2b" href="single-page.html">Ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span class="active"><i class="fa fa-list"></i></span>
+
+                        <h3>Otros Servicios</h3>
+
+                        <p>Observa nuestros otros servicios.</p>
+
+                        <div class="read_more">
+                            <a class="btn btn-2b" href="single-page.html">Ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <!-- end span_of_4 -->
+        </div>
+    </div>
+</div>
+
+<script>
+    jQuery(document).ready(function($){
+        window.dzsscr_init($('.mwrap-wrapper'),{
+            'type':'scrollTop'
+            ,'settings_skin':'skin_apple'
+            ,enable_easing: 'on'
+            ,settings_autoresizescrollbar: 'on'
+            ,settings_chrome_multiplier : 0.04
+        })
+    });
+
+</script>
+<link rel='stylesheet' type="text/css" href="<?php echo __PARALAX__?>fontawesome/font-awesome.min.css"/>
+<link rel='stylesheet' type="text/css" href="<?php echo __PARALAX__?>advancedscroller/plugin.css"/>
+<script src="<?php echo __PARALAX__?>advancedscroller/plugin.js" type="text/javascript"></script>
+<script src="<?php echo __PARALAX__?>dzsparallaxer/dzsparallaxer.js" type="text/javascript"></script>
+<script src="<?php echo __PARALAX__?>dzsscroller/scroller.js" type="text/javascript"></script>
+<!--<script src="dzslayouter/jquery.masonry.js" type="text/javascript"></script>-->
+<!--<script src="dzslayouter/jquery.masonry.ordered.js" type="text/javascript"></script>-->
+</body>
+</html>
