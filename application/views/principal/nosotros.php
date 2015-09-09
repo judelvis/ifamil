592f19b37bb7d754409626e980a941b06954c141
judelvis <jud.prog@gmail.com>
2015-09-09 16:51:18 -0430

    configuracion

--- application/views/principal/nosotros.php
+++ application/views/principal/nosotros.php
@@ -23,10 +23,8 @@
                 <div class="technology_list col-md-12">
                     <h2>Quienes somos?</h2>
 
-                    <p class="para">Viajes y Turismo IFAMIL C.A. es una filial del Instituto de Previsi?n Social de la
-                        Fuerza Armada Nacional (I.P.S.F.A.), establecida desde febrero de 1980, con el objeto de brindar
-                        un servicio de primera calidad, no solamente a la Fuerza Armada, sino tambi?n a los entes
-                        gubernamentales, privados y p?blico en general.</p>
+                    <p class="para">Viajes y Turismo IFAMIL C.A. es una filial del Instituto de Previsión Social de la Fuerza Armada Nacional (I.P.S.F.A.), establecida desde febrero de 1980, con el objeto de brindar un servicio de primera calidad, no solamente a la Fuerza Armada, sino también a los entes gubernamentales, privados y público en general.
+                    </p>
 
                     <div class="clearfix"></div>
                 </div>
@@ -41,8 +39,7 @@
             <div class="technology_list col-md-12">
                 <h2>Nuestro compromiso</h2>
 
-                <p class="para"> Brindar el mejor servicio en lo que a viajes tur?sticos y de negocios se refiere,
-                    con la finalidad de satisfacer las necesidades de nuestra distinguida clientela.</p>
+                <p class="para"> Brindar el mejor servicio en lo que a viajes turísticos y de negocios se refiere,  con la finalidad de satisfacer las necesidades de nuestra distinguida clientela.</p>
 
                 <div class="clearfix"></div>
             </div>
@@ -58,10 +55,7 @@
                 <h2>Mision</h2>
 
                 <div class="tech_para">
-                    <p class="para">Prestar los mejores servicios en viajes y turismo a nivel nacional e internacional
-                        con una alta calidad y responsabilidad, amparados con un excelente servicio de atenci?n al
-                        cliente y un personal altamente capacitado, manteniendo un horizonte claro del beneficio de
-                        bienestar social que deben recibir nuestros afiliados. </p>
+                    <p class="para"> Prestar los mejores servicios en viajes y turismo a nivel nacional e internacional con una alta calidad y responsabilidad, amparados con un excelente servicio de atención al cliente y un personal altamente capacitado, manteniendo un horizonte claro del beneficio de bienestar social que deben recibir nuestros afiliados. </p>
                 </div>
                 <div class="images_1_of_4 bg1 pull-right">
                     <span class="bg"><i class="fa fa-plane"></i> </span>
@@ -72,10 +66,7 @@
                 <h2>Vision</h2>
 
                 <div class="tech_para">
-                    <p class="para">Ser la agencia de viajes y turismo m?s competitiva y confiable del mercado, que
-                        ofrezca un alto nivel de calidad en sus servicios y beneficio social, con el prop?sito de
-                        satisfacer las necesidades de sus clientes tanto en el sector militar, gubernamental como
-                        p?blico en general. </p>
+                    <p class="para">Ser la agencia de viajes y turismo más competitiva y confiable del mercado, que ofrezca un alto nivel de calidad en sus servicios y beneficio social, con el propósito de satisfacer las necesidades de sus clientes tanto en el sector militar, gubernamental como público en general. </p>
                 </div>
                 <div class="images_1_of_4 bg1">
                     <span class="bg"><i class="fa fa-globe"></i> </span>
