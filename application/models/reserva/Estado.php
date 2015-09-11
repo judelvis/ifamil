05ccaecf5de7a08ba4223f36af93058965a7505c
crash <crash@localhost.localdomain>
2015-09-11 14:13:19 -0430

    Controller, Paquetes

--- application/models/reserva/Estado.php
+++ application/models/reserva/Estado.php
@@ -12,7 +12,7 @@ class Estado extends CI_Model {
 	 * Sitio: El lugar que ocupa una cosa.
 	 * Definicion de elementos del Origen y Destino del sistema
 	 */
-	private $tbl = 't_sitios';
+	private $tbl = 't_estado';
 	public $identificador = NULL;
 	public $estado;
 	
