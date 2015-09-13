29a652b641797511cbfdb638fddd8a11eaf3ca7c
judelvis <jud.prog@gmail.com>
2015-09-13 15:59:04 -0430

    configuracion

--- application/views/panel/incluir/menu.php
+++ application/views/panel/incluir/menu.php
@@ -3,11 +3,11 @@
 		<div id="left">
 			
 			<ul id="menu" class="collapse">
-				<li class="panel "><a href="<?php echo site_url("panel/principal") ?>"> <i class="icon-table"></i>Portafolio</a></li>
+				<li class="panel "><a href="<?php echo site_url("panel/principal") ?>"> <i class="icon-suitcase"></i>Paquetes</a></li>
 				<li class="panel "><a href="<?php echo site_url("panel/agregarGaleria") ?>"> <i class="icon-camera"></i>Galeria</a></li>
-				<li class="panel "><a href="<?php echo site_url("panel/noticia") ?>"> <i class="icon-table"></i>Noticias</a></li>
-				<li class="panel "><a href="<?php echo site_url("panel/tipo") ?>"> <i class="icon-table"></i>Categoria</a></li>
-                <li class="panel "><a href="<?php echo site_url("panel/empresa") ?>"> <i class="icon-table"></i>Empresas</a></li>
+				<li class="panel "><a href="<?php echo site_url("panel/noticia") ?>"> <i class="icon-file-text"></i>Noticias</a></li>
+				<li class="panel "><a href="<?php echo site_url("panel/tipo") ?>"> <i class="icon-th-large"></i>Categoria</a></li>
+                <li class="panel "><a href="<?php echo site_url("panel/empresa") ?>"> <i class="icon-building"></i>Empresas</a></li>
 				<li><a href="<?php echo site_url("panel/cerrar") ?>"><i class="icon-signin"></i> Salir </a></li>
 
 			</ul>
